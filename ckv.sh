37cba054ba301f510c96a50e07ca60fc6d08987d
Thomas Plisson <tom@upwindconsulting.net>
2022-11-08 18:09:34 -0500

    2022-11-08

--- ckv.sh
+++ ckv.sh
@@ -1,12 +1,17 @@
-CASE=case$1
-CMD="checkov -f $CASE.tf --external-checks-dir . -c $CASE --compact"
-echo "### Executing: $CMD"
-$CMD
-
+#!/bin/bash
 # CLI usage
-if [ $# -lt 2 ]
+if [ $# -lt 1 ]
 then
   echo 1>&2 "$0: not enough arguments\n"
   echo "Usage: ckv.sh <check_id>\n"
   exit 1
 fi
+
+# Variables settings
+ID=$1
+
+CMD="checkov -f case$ID.tf --external-checks-dir . -c case$ID --compact"
+echo "### Executing: $CMD"
+
+# Checkov scan
+$CMD
